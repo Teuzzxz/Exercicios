dd9efa086627358f52fe3119ec8adc19cd17208c
Teuzzxz <mateusramosmrpc@gmail.com>
2024-07-06 16:49:00 -0300

    Nem fudendo que deu certo

--- Projetos/Menu de interação 4/codigos.js
+++ Projetos/Menu de interação 4/codigos.js
@@ -3,6 +3,8 @@ let cont = 0
 const otlado = document.querySelector('div.otlado')
 const LIs = [...document.getElementsByTagName('li')]
 const uls = document.getElementsByTagName('ul')[0]
+const botaoLinha = [...document.getElementsByClassName('line')]
+
 
 BotaoFeito.addEventListener('click' , ()=>{
     if(cont%2==0){
@@ -13,11 +15,32 @@ BotaoFeito.addEventListener('click' , ()=>{
             v.style.transform = `translate(-125px)`
             v.style.transition = `${alternar(p)}s`
         })
+
+        botaoLinha.map((v,p)=>{
+            if(p==0){
+                v.style.transform= 'rotate(-40deg)'
+            }else if(p==1){
+                v.style.transform= 'rotate(40deg) translate(-4px , -5px)'
+            } else if (p==2){
+                v.classList.toggle('sumir') 
+            }
+        })
+
     }else{
         otlado.style.transform = 'translateX(0px)'
         cont++
         LIs.map((v,p)=>{
             v.style.transform = 'translate(0px)'
+        })
+
+        botaoLinha.map((v,p)=>{
+            if(p==0){
+                v.style.transform = ''
+            }else if(p==1){
+                v.style.transform= ''
+            } else if (p==2){
+                v.classList.toggle('sumir') 
+            }
         })
     }
     
