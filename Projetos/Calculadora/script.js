231cbe7689bcf32f5e967e9fe47b30e963a960bc
Teuzzxz <mateusramosmrpc@gmail.com>
2024-07-11 19:07:33 -0300

    dps dessa vo ate pro juli

--- Projetos/Calculadora/script.js
+++ Projetos/Calculadora/script.js
@@ -36,12 +36,18 @@ const operações = [
 
 num.map((v, p) => {
     v.addEventListener('click', () => {
-        if (typeof res[res.length - 1] == 'number') {
-            console.log('é string')
-        }
-        res.push(Number(v.innerHTML))
-        Lres.innerHTML += v.innerHTML
-        console.log(res)
+            if (typeof res[res.length - 1] == 'number') {
+                res[res.length - 1] = res[res.length - 1] + v.innerHTML
+                res[res.length - 1] = Number(res[res.length - 1])
+                Lres.innerHTML += v.innerHTML 
+                console.log(res)
+            } else {
+                res.push(Number(v.innerHTML))
+                Lres.innerHTML += v.innerHTML
+                console.log(res)
+            }
+
+    
     })
 })
 
@@ -55,8 +61,8 @@ ope.map((v,p) => {
 
 
 const Apenas1 = (val) => {
-    if (typeof res[res.length - 1]=='string' || res.length ==0) {
-        console.log('deu')
+    if (typeof res[res.length - 1] == 'string' || res.length == 0) {
+        console.log(res)
     } else {
         res.push(val)
         Lres.innerHTML += val
@@ -68,3 +74,5 @@ const Apenas1 = (val) => {
 
 
 
+// console.log('div toda: ' + res)
+//             console.log('Numero atual ' + Number(num[p].innerHTML))
