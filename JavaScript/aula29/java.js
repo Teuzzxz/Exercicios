04ed87ed6ddd549156cb538b64db986a86132923
Teuzzxz <mateusramosmrpc@gmail.com>
2024-07-17 14:29:36 -0300

    Muito aprendizado slc

---
+++ JavaScript/aula29/java.js
@@ -0,0 +1,9 @@
+// const randomico = () => {
+//     let num = parseInt(Math.random() * 100)
+//     console.log(num);
+// }
+// setInterval(randomico,1000)
+
+let teste = Math.cos
+
+console.log(teste);
