b3b8f35e0cc90feb4109b3397c0e01d9c94f60f2
Teuzzxz <mateusramosmrpc@gmail.com>
2024-07-19 12:45:52 -0300

    mais tr

--- teste.js
+++ teste.js
@@ -1,2 +1,14 @@
-let teste = -1
+const exemplo = {
+    ex1: 1,
+    ex2: 2,
+}
+const exemplo1 = {
+    ex3: 3,
+    ex4: 4,
+}
+const exemplo2 = Object.assign(exemplo,exemplo1)
 
+delete(exemplo2.ex1)
+
+
+console.log(exemplo2);
