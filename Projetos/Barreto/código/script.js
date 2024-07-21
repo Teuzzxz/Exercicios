52665903a74e517dbd5815cadd792414bcce9932
Teuzzxz <mateusramosmrpc@gmail.com>
2024-07-21 20:17:44 -0300

    não saí do lugar

--- Projetos/Barreto/código/script.js
+++ Projetos/Barreto/código/script.js
@@ -1,11 +1,4 @@
 const Menu = [...document.getElementsByClassName('animation')]
-const linhas = [...document.getElementsByClassName('lineJs')]
+const linhas = [...document.getElementsByClassName('line')]
+let PrimeiraVez = true
 
-
-Menu.map((v,p) => {
-    v.addEventListener('click', () => {
-        linhas[p].classList.add('line')
-        let c = document.getElementsByClassName('line')
-        c[c.length - 1].setAttribute('style' , `width:${Menu[p].offsetWidth}px`)
-    })
-})
