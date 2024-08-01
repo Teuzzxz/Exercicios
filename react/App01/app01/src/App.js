bfad028f3aebdc3b0ed8bc72a3847431a30c379d
Teuzzxz <mateusramosmrpc@gmail.com>
2024-08-01 16:05:40 -0300

    toma lerda

---
+++ react/App01/app01/src/App.js
@@ -0,0 +1,11 @@
+import "./Style.css"
+
+export default function App() {
+	return (
+		<>
+			<main>
+				<p>teuz code testando</p>
+			</main>
+		</>
+	)
+}
---
+++ react/App01/app01/src/testando2.jsx
@@ -0,0 +1,9 @@
+import "./testando2.css"
+
+export default function resto() {
+	;<>
+		<div>
+			<p>teuz lindo</p>
+		</div>
+	</>
+}
