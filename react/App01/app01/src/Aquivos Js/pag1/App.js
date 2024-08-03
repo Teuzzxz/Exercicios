91aa5ee321ca96be4356ea5ecff86e2722072f3c
Teuzzxz <mateusramosmrpc@gmail.com>
2024-08-03 15:19:54 -0300

    a

--- react/App01/app01/src/Aquivos Js/pag1/App.js
+++ react/App01/app01/src/Aquivos Js/pag1/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import "../pag1/style.css"
 import HeaderC from "./HeaderC/HeaderC"
+import Corpo from "./HeaderC/corpo"
 
 export default function App() {
 	return (
 		<>
 			<HeaderC />
+			<Corpo />
 		</>
 	)
 }
---
+++ react/App01/app01/src/Aquivos Js/pag1/HeaderC/corpo.js
@@ -0,0 +1,14 @@
+export default function corpo() {
+	return (
+		<>
+			<main>
+				<div>
+					<h1>Ola</h1>
+				</div>
+				<div>
+					<h1>bem vindo</h1>
+				</div>
+			</main>
+		</>
+	)
+}
