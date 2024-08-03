24d5dc56001d40a09f30efeb837b4c9470ed1526
Teuzzxz <mateusramosmrpc@gmail.com>
2024-08-03 15:04:19 -0300

    a

--- react/App01/app01/src/Aquivos Js/pag1/HeaderC/HeaderC.js
+++ react/App01/app01/src/Aquivos Js/pag1/HeaderC/HeaderC.js
@@ -1,12 +1,12 @@
 import React from "react"
 import "../HeaderC/styleHeader.css"
-import DC from "./DC"
+import Topo from "./componente1"
 
 export default function HeaderC() {
 	return (
 		<header>
 			<h4> Bem vindo taxade</h4>
-			<DC />
+			<Topo />
 		</header>
 	)
 }
