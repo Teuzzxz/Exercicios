022f5dd909a9a260197ef66a96bebb31db6f016a
Teuzzxz <mateusramosmrpc@gmail.com>
2024-08-28 15:15:12 -0300

    -

--- react/App01/app01/src/Aquivos Js/pag1/teste.js
+++ react/App01/app01/src/Aquivos Js/pag1/teste.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from "react"
 
 export default function Nome(props) {
-	const [num, setnum] = useState(0)
-	const add = () => {
-		setnum(num + 1)
-	}
+	const lista = ["mateus", "ramos", "dos", "santos"]
 
 	return (
 		<>
-			<button onClick={add}>click for add</button>
-			<p>{num}</p>
-			{num > 10 && num < 15 && <p>toma ta lerda</p>}
+			{lista.map((e, i) => {
+				return <p>{e}</p>
+			})}
 		</>
 	)
 }
