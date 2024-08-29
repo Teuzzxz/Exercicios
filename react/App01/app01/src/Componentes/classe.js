6b10ec0cd0565192b75f17ea0585a0accdacc453
Teuzzxz <mateusramosmrpc@gmail.com>
2024-08-29 13:56:01 -0300

    -

--- react/App01/app01/src/Componentes/classe.js
+++
@@ -1,8 +0,0 @@
-export default class cadastro {
-	constructor() {
-		this.elemento = document.createElement("div")
-		this.nome = document.createElement("p")
-		this.email = document.createElement("p")
-		this.senha = document.createElement("p")
-	}
-}
--- react/App01/app01/src/Componentes/login.js
+++ react/App01/app01/src/Componentes/login.js
@@ -1,14 +1,46 @@
 import React from "react"
+import Lixeira from "../assets/lixeira.png"
 
 export default function Paglogin() {
+	const users = [
+		{
+			id: "damwodm2321",
+			name: "Mateus",
+			age: 15,
+			email: "mate@gmail.com",
+		},
+		{
+			id: "damwoadawdm2321",
+			name: "André",
+			age: 23,
+			email: "dede@gmail.com",
+		},
+	]
+
 	return (
 		<>
 			<div className="Login">
-				<h1>Cadastro de usuários</h1>
-				<input></input>
-				<input></input>
-				<input></input>
-				<button>Cadastrar</button>
+				<form>
+					<h1>Cadastro de usuários</h1>
+					<input name="nome" type="text"></input>
+					<input name="idade" type="number"></input>
+					<input name="email" type="email"></input>
+					<button>Cadastrar</button>
+				</form>
+				{users.map((e, p) => {
+					return (
+						<div style={{ display: "flex", backgroundColor: "grey", justifyContent: "space-between", alignItems: "center" }}>
+							<div>
+								<p>Nome: {users[p].name}</p>
+								<p>Idade: {users[p].age}</p>
+								<p>Email: {users[p].email}</p>
+							</div>
+							<a src="#">
+								<img src={Lixeira} />
+							</a>
+						</div>
+					)
+				})}
 			</div>
 		</>
 	)
