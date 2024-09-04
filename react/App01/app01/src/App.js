c3607002cff25c48dee724bd222ef82db28485f5
Teuzzxz <mateusramosmrpc@gmail.com>
2024-09-04 16:12:49 -0300

    -

--- react/App01/app01/src/App.js
+++ react/App01/app01/src/App.js
@@ -1,11 +1,14 @@
 import React from "react"
 import "./style.css"
-import Teste from "./Componentes/testes.js"
+import Teste from "./Componentes/notas.js"
+import Props from "./Componentes/propsChildren.js"
 
 export default function App() {
 	return (
 		<>
-			<Teste />
+			<Props>
+				<p>Filhote</p>
+			</Props>
 		</>
 	)
 }
---
+++ react/App01/app01/src/Componentes/propsChildren.js
@@ -0,0 +1,8 @@
+export default function Daora(props) {
+	return (
+		<>
+			<h1>ata</h1>
+			{props.children}
+		</>
+	)
+}
