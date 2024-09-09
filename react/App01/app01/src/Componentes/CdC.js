b7f807d8125e6e6156c21ecadcc2d0a9dc88e621
Teuzzxz <mateusramosmrpc@gmail.com>
2024-09-09 14:50:30 -0300

    -

--- react/App01/app01/src/Componentes/CdC.js
+++ react/App01/app01/src/Componentes/CdC.js
@@ -3,12 +3,19 @@ import React, { useState } from "react"
 export default class Classe extends React.Component {
 	constructor(props) {
 		super(props)
-		this.num = useState(0)
+		this.num = {
+			numero: 0,
+		}
+	}
+	adi() {
+		this.setState({ numero: this.num.numero + 1 })
+		console.log(this.num.numero)
 	}
 	render() {
 		return (
 			<>
-				<h1>Contagem: {this.num}</h1>
+				<h1>Contagem: {this.num.numero}</h1>
+				<button onClick={() => this.adi()}>Adicionar</button>
 			</>
 		)
 	}
